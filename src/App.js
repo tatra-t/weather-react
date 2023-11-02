2fc07710964ca378d0cab889d6a46d3c95372b3a
tatra-t <tanechka.dush@gmail.com>
2023-11-02 20:29:35 +0100

    fix axios error

--- src/App.js
+++ src/App.js
@@ -1,19 +1,46 @@
-import React, { useState } from "react";
-import axios from "axios";
 import "./App.css";
+import axios from "axios";
+import React, { useState } from "react";
+
+
 
 export default function App() {
   let [city, setCity] = useState("");
-  let [text, setText] = useState("");
-  let [temperature, setTemperature] = useState(null);
-  let [humidity, setHumidity] = useState(null);
-  let [wind, setWind] = useState(null);
-  let [description, setDescription] = useState(null);
-  let [icon, setIcon] = useState(null);
+  let [temperature, setTemperature] = useState("");
+  let [humidity, setHumidity] = useState("");
+  let [wind, setWind] = useState("");
+  let [description, setDescription] = useState("");
+  let [icon, setIcon] = useState("");
 
-  function HandleSubmit(event) {
+  function showWeather(response) {
+    setTemperature(response.data.main.temp);
+    setHumidity(response.data.main.humidity);
+    setDescription(response.data.weather[0].description);
+    setWind(response.data.wind.speed);
+    setIcon(
+      `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
+  }
+  function updateCity(event) {
     event.preventDefault();
-    setText(
+    setCity(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=5bd8dd5876af31be7dd1dd4666c7f2a5&units=metric`;
+    axios.get(url).then(showWeather);
+  }
+ 
+
+  return (
+    
+    <div className="App">
+      <h1>Weather Search Engine </h1>
+      <form onSubmit={handleSubmit}>
+        <input type="Search" placeholder="Enter a city" onChange={updateCity} />
+        <input type="submit" value="Search" />
+      </form>
       <div>
         It is in {city}
         <ul>
@@ -26,33 +53,7 @@ export default function App() {
           </li>
         </ul>
       </div>
-    );
-  }
-  function updateCity(event) {
-    event.preventDefault();
-    setCity(event.target.value);
-  }
-  function showWeather(response) {
-    setTemperature(response.data.main.temp);
-    setHumidity(response.data.main.humidity);
-    setDescription(response.data.weather[0].description);
-    setWind(response.data.wind.speed);
-    setIcon(
-      `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-    );
-  }
-
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=5bd8dd5876af31be7dd1dd4666c7f2a5&units=metric`;
-  axios.get(url).then(showWeather);
-  console.log(url);
-  return (
-    <div className="App">
-      <h1>Weather Search Engine </h1>
-      <form onSubmit={HandleSubmit}>
-        <input type="Search" placeholder="Enter a city" onChange={updateCity} />
-        <input type="submit" value="Search" />
-      </form>
-      {text}
     </div>
-  );
+  )
+
 }
