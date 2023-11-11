5255d64cda9f7b977f12eb285e5d9f8dd4b9ad72
tatra-t <tanechka.dush@gmail.com>
2023-11-11 19:32:48 +0100

    add Weather today

--- src/App.js
+++ src/App.js
@@ -1,59 +1,99 @@
-import "./App.css";
-import axios from "axios";
 import React, { useState } from "react";
+import 'bootstrap/dist/css/bootstrap.css';
+import "./App.css"
+import axios from "axios";
 
 
 
 export default function App() {
-  let [city, setCity] = useState("");
-  let [temperature, setTemperature] = useState("");
-  let [humidity, setHumidity] = useState("");
-  let [wind, setWind] = useState("");
-  let [description, setDescription] = useState("");
-  let [icon, setIcon] = useState("");
 
-  function showWeather(response) {
-    setTemperature(response.data.main.temp);
-    setHumidity(response.data.main.humidity);
-    setDescription(response.data.weather[0].description);
-    setWind(response.data.wind.speed);
-    setIcon(
-      `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-    );
-  }
-  function updateCity(event) {
-    event.preventDefault();
-    setCity(event.target.value);
-  }
+        let [city, setCity] = useState("");
+        let [temperature, setTemperature] = useState("");
+        let [temperatureFeel, setTemperatureFeel] = useState("");
+        let [temperatureMin, setTemperatureMin] = useState("");
+        let [temperatureMax, setTemperatureMax] = useState("");
+        let [humidity, setHumidity] = useState("");
+        let [wind, setWind] = useState("");
+        let [description, setDescription] = useState("");
+        let [icon, setIcon] = useState("");
+        let [nameCity, setNameCity] = useState("");
+      
+        function showWeather(response) {
+          setTemperature(Math.round(response.data.main.temp));
+          setTemperatureFeel(Math.round(response.data.main.feels_like));
+          setTemperatureMin(Math.round(response.data.main.temp_min));
+          setTemperatureMax(Math.round(response.data.main.temp_max));
+          setNameCity(response.data.name);
+          setHumidity(response.data.main.humidity);
+          setDescription(response.data.weather[0].description);
+          setWind(response.data.wind.speed);
+          setIcon(
+            `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+          );
+        }
+        function updateCity(event) {
+          event.preventDefault();
+          setCity(event.target.value);
+        }
+      
+        function handleSubmit(event) {
+          event.preventDefault();
+          let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=5bd8dd5876af31be7dd1dd4666c7f2a5&units=metric`;
+          axios.get(url).then(showWeather);
+        }
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=5bd8dd5876af31be7dd1dd4666c7f2a5&units=metric`;
-    axios.get(url).then(showWeather);
-  }
- 
-  return (
-    
-    <div className="App">
-      <h1>Weather Search Engine </h1>
-      <form onSubmit={handleSubmit}>
-        <input type="Search" placeholder="Enter a city" onChange={updateCity} />
-        <input type="submit" value="Search" />
-      </form>
-      <div>
-        It is in {city}
-        <ul>
-          <li>Temperatura: {Math.round(temperature)} °C</li>
-          <li>Humidity: {humidity} %</li>
-          <li>Wind: {Math.round(wind)} km/h</li>
-          <li>Description: {description}</li>
-          <li>
-            <img src={icon} alt="" />
-          </li>
-        </ul>
+    return (
+    <div className = "code">
+        <div className="container">
+        <nav className="navbar bg-light">
+            <div className="container-fluid">
+                <a className="navbar-brand">Weather in World</a>
+                <form className="d-flex" role="search" onSubmit={handleSubmit}>
+                    <input className="form-control me-2" type="search" placeholder="Search city" aria-label="Search" onChange={updateCity}/>
+                    <button className="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
+            <div className="col-8"></div>
+            <div className="col-4 curLoc">
+            <a href="">select current location </a></div> 
+        </nav>
+
+<div className="today">
+                <h1>{nameCity}</h1>
+                <h2 id="dateNow"> </h2>
+                <h2 id="time"> </h2>
+        </div>
+
+        <div className="row ">
+            <div className="col-6 now1">
+                <span className="now"> <span id="tempCel"> {temperature}</span> ° <span class="celsius">C</span> 
+                 | <span className="fahrenheit"> <a href="">F</a> </span>
+                </span>
+                <br/>
+                <img className="imgMain" src={icon} alt="" id="emojiMain"/>
+
+                <br/>
+                <small> feels like <span id="feelLike"> {temperatureFeel}</span>°</small>
+            </div>
+            <div className="col-6 now2">
+                <span id="weekDay"> </span>
+                <br />
+                <span className="now3"> <span id="tempCurMax">{temperatureMax}</span>° / <span id="tempCurMin">{temperatureMin}</span> ° </span>
+                <br /> <hr/> <span id="main"> </span>{description} <br /> Wind <span id="wind">{wind} </span> m/s <br />
+                Humidity <span id="humidity">{humidity}</span> %
+            </div>
+        </div>
+          <div className="ForecastWeather">
+          <div className="col-12 hourly">Hourly</div>
+          <div  id="forecastHour"></div>
+          
+          <div className="col-12 days">Daily</div>
+          <div id= "forecastDay"> </div>
+          <br />
       </div>
-      <a href="https://github.com/tatra-t/weather-react/tree/master/src">My github</a>
+      </div>
+        <a href="">Open-source code</a> by Tetiana Dushenko
     </div>
-  )
-  }
 
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-import App2 from './App2';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-    <App2/>
+    <App/>
   </React.StrictMode>
 );
 
